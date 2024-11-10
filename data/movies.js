b23f8552d10b1de4a6c39896c16cd9e82c976d88
cormactaylor <cormacmtaylor@gmail.com>
2024-11-10 11:57:02 -0500

    movies.js initally written

--- data/movies.js
+++ data/movies.js
@@ -1,13 +1,40 @@
-//import axios, md5
+/*
+ * Cormac Taylor
+ * I pledge my honor that I have abided by the Stevens Honor System.
+ */
+import axios from "axios";
+import { isInvalidString } from "../helpers.js";
+
+const API_URL = "http://www.omdbapi.com/?apikey=CS546";
 
 export const searchMoviesByTitle = async (title) => {
-  /*Function to make an axios call to search the api and return up to 50 movies matching the title param
-  API endpoint: http://www.omdbapi.com/?apikey=CS546&s={title}
-  */
+  if (isInvalidString(title)) throw "title argument must be valid string!";
+
+  const SEARCH_URL = `${API_URL}&s=${title}`;
+  const searchResults = await axios.get(SEARCH_URL);
+
+  if (!searchResults.Response) return searchResults;
+
+  const totalPages = Math.ceil(searchResults.totalResults / 10);
+  const maxRequestPage = totalPages > 5 ? 5 : totalPages;
+  for (let i = 2; i <= maxRequestPage; i++) {
+    const { Search } = await axios.get(`${SEARCH_URL}&page=${i}`);
+    searchResults.Search.concat(Search);
+  }
+
+  return searchResults;
 };
 
 export const getMovieById = async (id) => {
-  /*Function to make an axios call to the the api matching the id
- API endpoint: http://www.omdbapi.com/?apikey=CS546&i={id}
-  */
+  if (isInvalidString(id)) throw "id argument must be valid string!";
+
+  const SEARCH_URL = `${API_URL}&i=${id}`;
+  const searchResults = await axios.get(SEARCH_URL);
+
+  if (!searchResults.Response) {
+    delete searchResults.Error;
+    return searchResults;
+  }
+
+  return searchResults;
 };
---
+++ helpers.js
@@ -0,0 +1,7 @@
+/*
+ * Cormac Taylor
+ * I pledge my honor that I have abided by the Stevens Honor System.
+ */
+const isInvalidString = (str) => {
+  return typeof str !== "string" || str.trim().length == 0;
+};
